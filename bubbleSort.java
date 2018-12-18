d8aef1a2ee9d5dc7935a506f8c62a8ebd0e9d8e8
JMR-jmr <45375796+JMR-jmr@users.noreply.github.com>
2018-12-18 09:34:13 +0800

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,1 +1,15 @@
-
+int[] test = {5,1,2,4,3};
+int temp = 0;
+int length = test.length;
+for(int i = 0; i < length; i++){
+  for(int j = 0; j <length - 1; j++)
+  {
+    if(test[i] > test[i + 1]){
+      temp = test[i];
+      test[i] = test[i + 1];
+      test[i + 1] = temp;
+    }
+  
+  }
+  length--;
+}
